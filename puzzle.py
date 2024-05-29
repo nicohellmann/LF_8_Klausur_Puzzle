48a72234de62dbb2cbbe08eee252c6ed913bf061
unknown <hellmann1.nico@gmx.de>
2024-05-29 13:04:08 +0200

    initial commit

---
+++ puzzle.py
@@ -0,0 +1,7 @@
+from puzzleteil import puzzleteil
+class puzzle:
+    
+    def puzzle(self,puzzleteile:list):
+        self.teile = puzzleteile
+
+    
---
+++ puzzleteil.py
@@ -0,0 +1,8 @@
+class puzzleteil:
+
+    def puzzleteil(self,links:int,unten:int,rechts:int):
+        self.linksunten =links
+        self.oben = unten
+        self.rechtsunten = rechts
+        self.gesamtesTeil = [links, unten, rechts]
+        
