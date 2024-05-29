4120a75a76d9bcbdb792ee4f2a9131455392d5c2
unknown <hellmann1.nico@gmx.de>
2024-05-29 14:49:09 +0200

    added function to rotate a puzzle piece

--- puzzleteil.py
+++ puzzleteil.py
@@ -1,8 +1,22 @@
 class puzzleteil:
 
     def puzzleteil(self,links:int,unten:int,rechts:int):
-        self.linksunten =links
-        self.oben = unten
-        self.rechtsunten = rechts
+        self.links =links
+        self.unten = unten
+        self.rechts = rechts
         self.gesamtesTeil = [links, unten, rechts]
-        
+
+    def drehen(self, richtung = "links"):
+        if richtung =="links":
+            temp = self.links
+            self.links = self.rechts
+            self.rechts = self.unten
+            self.unten = temp
+            self.gesamtesTeil = [self.links, self.unten, self.rechts]
+        else:
+            temp = self.links
+            self.links = self.unten
+            self.unten = self.rechts
+            self.rechts = temp
+
+
