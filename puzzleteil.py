64ba241f0b98e5a98553f3e776acb7920f22b23b
unknown <hellmann1.nico@gmx.de>
2024-05-30 12:44:41 +0200

    renamed attribute unten to mitte to better communicate position when flipped

--- puzzleteil.py
+++ puzzleteil.py
@@ -1,23 +1,23 @@
 class puzzleteil:
 
-    def puzzleteil(self,links:int,unten:int,rechts:int):
+    def puzzleteil(self,links:int,mitte:int,rechts:int):
         self.links =links
-        self.unten = unten
+        self.mitte = mitte
         self.rechts = rechts
-        self.gesamtesTeil = [links, unten, rechts]
+        self.gesamtesTeil = [links, mitte, rechts]
 
     def drehen(self, richtung = "links"):
         if richtung =="links":
             temp = self.links
             self.links = self.rechts
-            self.rechts = self.unten
-            self.unten = temp
-            self.gesamtesTeil = [self.links, self.unten, self.rechts]
+            self.rechts = self.mitte
+            self.mitte = temp
+            self.gesamtesTeil = [self.links, self.mitte, self.rechts]
         else:
             temp = self.links
-            self.links = self.unten
-            self.unten = self.rechts
+            self.links = self.mitte
+            self.mitte = self.rechts
             self.rechts = temp
-            self.gesamtesTeil = [self.links, self.unten, self.rechts]
+            self.gesamtesTeil = [self.links, self.mitte, self.rechts]
 
 
